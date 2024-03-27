d618fd946cc115a0c07cfefd6ecdb0261febc657
risav pyakurel <pyakurel.risav@gmail.com>
2024-03-27 14:40:45 +0545

    nothing

--- myapp/myapp/views.py
+++ myapp/myapp/views.py
@@ -4,9 +4,31 @@ import datetime
 
 def home(request):
   date= datetime.datetime.now()
+  isActive =True
+  name = "Momo and Code"
+  list_of_programs= [
+    'WAP to check the prime or composite',
+    'WAP to check the odd or even',
+    'WAP to print pascal triangles',
+    'wap to print all prime numbers from 1 to 100'
+  ]
+
+  student={
+    'student_name' : "Risav",
+    'student_college': "Amrit Science Campus ",
+    'student_city': "Kathmandu"
+  }
   print("test function is called from view")
   #return HttpResponse(" <h1> Hello this is index page  </h1> " +str(date))
-  return render(request,"home.html",{})
+  data= {
+    'date':date,
+    'isActive':isActive,
+    'name': name,
+    'list_of_programs': list_of_programs,
+    'student_data' : student
+
+  }
+  return render(request,"home.html",data)
 
 def about(request):
   #return HttpResponse(" <h1> <i>This is about page</i></h1>")
