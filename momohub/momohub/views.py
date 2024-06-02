507762596a51f5b56e554126409e9c98c59ec37b
risav pyakurel <pyakurel.risav@gmail.com>
2024-06-02 23:13:01 +0545

    templating

--- momohub/momohub/views.py
+++ momohub/momohub/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello, OOMFIES. You are at momohub")
+    #return HttpResponse("<h1> Hello, OOMFIES. You are at momohub")
+    return render(request, 'website/index.html')
 
 
 def about(request):
     return HttpResponse("<h1> Momohub is all about finding best momo place is Nepal")
 
 def contact(request):
-    return HttpResponse("Please contact us to get featured in our site")
+    return HttpResponse("<h1>Please contact us to get featured in our site")
