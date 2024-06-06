f9f950056cac0ba998168e9f2c7d8d2c3dd7f8a0
risav pyakurel <pyakurel.risav@gmail.com>
2024-06-06 23:30:39 +0545

    handling model

--- momohub/momo/models.py
+++ momohub/momo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class MomoVarity(models.Model):
+    MOMO_TYPE_CHOICE= [
+        ('SM', 'STEAM MOMO')
+        ('FM','Fried MOMO')
+        ('CM', 'Chilliy MOMO')
+    ]
+    name = models.CharField(max_length= 100)
+    image = models.ImageField(upload_to='momo/')
+    date_added = models.DateTimeField(default= timezone.now)
+    type=  models.CharField(max_length= 2,choices= MOMO_TYPE_CHOICE)
+
